393f793dd931c91aaa977fa02af6dd339ea66223
JeLo <jett@hotmail.de>
2017-11-03 05:46:05 +0100

    added elasticsearch

---
+++ src/main/java/com/jhproj/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.jhproj.repository.search;
+
+import com.jhproj.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
